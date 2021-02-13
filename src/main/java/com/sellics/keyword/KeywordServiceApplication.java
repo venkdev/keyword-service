e2172ea21b5477074daae64b85a80e24da9393a2
Venkatesh <venkatesh92r@gmail.com>
2021-02-13 20:05:11 +0530

    Initial commit.

---
+++ src/main/java/com/sellics/keyword/KeywordServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sellics.keyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeywordServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeywordServiceApplication.class, args);
+	}
+
+}
